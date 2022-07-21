a5eaa9cd3b5daea854600439dcf8a9c75c0eb99f
TranDangKhoi <88824627+TranDangKhoi@users.noreply.github.com>
2022-07-22 00:19:51 +0700

    Demo To Do List

---
+++ to-do-list (localStorage excersise)/todo.js
@@ -0,0 +1,31 @@
+window.addEventListener("load", function () {
+  const toDoForm = document.querySelector(".todo-form");
+  const toDoList = document.querySelector(".todo-list");
+  let toDos = localStorage ? JSON.parse(localStorage.getItem("toDoList")) : [];
+  console.log(Array.isArray(toDos));
+  if (toDos.length > 0) {
+    [...toDos].forEach((item) => createToDoItem(item));
+  }
+  function createToDoItem(toDoVal) {
+    const todoItemTemplate = `
+      <div class="todo-item">
+              <span class="todo-text">${toDoVal}</span>
+              <i class="fa-solid fa-trash todo-remove"></i>
+          </div>
+      `;
+    toDoList.insertAdjacentHTML("beforeend", todoItemTemplate);
+  }
+  toDoForm.addEventListener("submit", function (e) {
+    e.preventDefault();
+    // Lấy ra value từ input
+    const toDoVal = this.elements["todo"].value;
+    createToDoItem(toDoVal);
+    // save to localStorage
+    // Mỗi lần add một cái sẽ ra một cái -> không thể lưu từng biến được mà phải lưu thành mảng
+    toDos.push(toDoVal);
+    // console.log("log ~ toDos", toDos);
+    localStorage && localStorage.setItem("toDoList", JSON.stringify(toDos));
+    console.log(toDoVal);
+    this.elements["todo"].value = "";
+  });
+});
