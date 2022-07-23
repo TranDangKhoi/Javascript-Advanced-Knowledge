5763f6ed45c3ef451cbbe4a49e4f9a490724b290
TranDangKhoi <88824627+TranDangKhoi@users.noreply.github.com>
2022-07-23 15:58:56 +0700

    Email Validation and update Live Website (will make it prettier later)

---
+++ email-validation (regex excersise)/validate.js
@@ -0,0 +1,17 @@
+// Email Validation
+// abc@gmail.com abc@yahoo.com : valid
+// abcdk ktois123 : invalid
+const emailInput = document.querySelector(".input");
+emailInput.addEventListener("input", function (e) {
+  const value = e.target.value;
+  //   console.log(value);
+  const emailRegex =
+    /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  if (emailRegex.test(value.trim())) {
+    e.target.classList.add("valid");
+    e.target.classList.remove("invalid");
+  } else {
+    e.target.classList.add("invalid");
+    e.target.classList.remove("valid");
+  }
+});
